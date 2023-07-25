e46fb58921987523179080ebdabb4f40e9497882
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-25 16:39:37 -0300

    fix(main.ts): altera o caminho de roteamento do SwaggerModule de 'api' para '/' O caminho de roteamento do SwaggerModule foi alterado de 'api' para '/' para melhorar a consistência com a estrutura de roteamento do aplicativo.

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .setVersion(version)
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
