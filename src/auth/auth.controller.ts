9bf411d8f15db885188b0f221fd5512d9f04009e
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-26 14:20:57 -0300

    feat(auth): adiciona suporte para registro de usuário fix(auth): adiciona a tag @ApiTags('Authentication') para documentação Swagger fix(auth): adiciona a classe SignUpDto para representar os dados de registro de usuário fix(auth): adiciona o endpoint de registro de usuário na rota '/auth/register' fix(auth): implementa a lógica de registro de usuário no método signUp da AuthService
    As alterações foram feitas para adicionar suporte ao registro de usuário no sistema de autenticação. Foi adicionada a classe SignUpDto para representar os dados necessários para o registro de usuário. Além disso, foi adicionado o endpoint '/auth/register' no AuthController para lidar com o registro de usuário. A lógica de registro de usuário foi implementada no método signUp da AuthService, que verifica se o usuário já existe e, caso contrário, cria um novo usuário com os dados fornecidos.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiBearerAuth, ApiProperty } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 
 class SignInDto {
@@ -20,6 +20,21 @@ class SignInDto {
   password: string;
 }
 
+export class SignUpDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+}
+
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -28,6 +43,12 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('register')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
   }
 
   @ApiBearerAuth()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './auth.controller';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -18,5 +24,19 @@ export class AuthService {
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async signUp(signUpDto: SignUpDto): Promise<User> {
+    const user = await this.usersService.user(signUpDto.username);
+    if (user) {
+      throw new ConflictException();
+    }
+    const newUser = await this.usersService.createUser({
+      username: signUpDto.username,
+      password: signUpDto.password,
+      name: signUpDto.name,
+      email: signUpDto.email,
+    });
+    return newUser;
   }
 }
