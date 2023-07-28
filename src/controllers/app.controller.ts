a3ec86900b13ec7ede28cba763ea04449711b514
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-28 15:26:24 -0300

    chore(app.controller.ts, app.service.ts): remove arquivos app.controller.ts e app.service.ts Os arquivos app.controller.ts e app.service.ts foram removidos do projeto. Esses arquivos não eram mais necessários e foram excluídos para manter o código limpo e organizado.

--- src/controllers/app.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from 'src/services/app.service';
-
-@Controller('app')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-}
