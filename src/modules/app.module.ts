2fa570fc897ee33543644ddc71f9cb3b7cffa407
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-15 11:34:29 -0300

    feat(app.module.ts): adiciona ConfigModule.forRoot() como importação Adiciona o módulo ConfigModule.forRoot() como uma importação no AppModule. Isso permite que o aplicativo utilize a biblioteca de configuração do NestJS para carregar variáveis de ambiente e arquivos de configuração. Essa adição é útil para fornecer uma maneira mais flexível de configurar o aplicativo e gerenciar suas configurações.

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,8 +4,16 @@ import { InstanceModule } from './instance.module';
 import { MessageModule } from './message.module';
 import { ChatModule } from './chat.module';
 import { TokenModule } from './token.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, InstanceModule, MessageModule, ChatModule, TokenModule],
+  imports: [
+    AuthModule,
+    InstanceModule,
+    MessageModule,
+    ChatModule,
+    TokenModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
