3a24237d5664a76ff52ff97d5093f63fdfb3675b
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-20 19:42:19 -0300

    chore: increase maximum payload size

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { version } from '../package.json';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/app.module';
 import { InstanceService } from './services/instance.service';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,8 +24,10 @@ async function bootstrap() {
   SwaggerModule.setup('/', app, document);
 
   app.enableCors();
-
   app.get(InstanceService).restoreSessions();
+
+  app.use(bodyParser.json({ limit: '20mb' }));
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
